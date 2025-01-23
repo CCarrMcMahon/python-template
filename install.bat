de10c4c029b41f549d2725082a6769736d86ec1b
Christopher McMahon <CCarrMcMahon@gmail.com>
2025-01-23 08:48:56 -0600

    Move configuration deletion and temp file creation to end of install script to avoid leaving temp file or deleting file early

--- install.bat
+++ install.bat
@@ -1,33 +1,8 @@
 @echo off
-setlocal enabledelayedexpansion
-
-REM Create a temporary batch file to store the main script
-set "this_file=%~f0"
-set "temp_file=%~dp0temp.bat"
-set "copy_lines=0"
-
-REM Copy the main script to the temporary batch file
-echo @echo off>"%temp_file%"
-echo.>>"%temp_file%"
-for /f "delims=" %%i in ('findstr /n "^" "%this_file%"') do (
-    set "line=%%i"
-    set "line=!line:*:=!"
-    if "!line!"==":start_main_script" (
-        set "copy_lines=1"
-    ) else if "!line!"==":end_main_script" (
-        set "copy_lines=0"
-    ) else if !copy_lines! equ 1 (
-        if "!line!"=="" (
-            echo.>>"%temp_file%"
-        ) else (
-            echo !line!>>"%temp_file%"
-        )
-    )
-)
 
 REM Execute the configure_template.py script
 python configure_template.py
-del configure_template.py
+echo.
 
 :start_main_script
 REM Prompt user for optional dependencies
@@ -58,5 +33,31 @@ if /i "%tests%"=="y" pip install -e .[tests]
 echo Setup complete. Virtual environment created and dependencies installed.
 :end_main_script
 
-REM Replace the current batch file with the temporary batch file
+REM Create a temporary batch file to store the main script
+setlocal enabledelayedexpansion
+set "this_file=%~f0"
+set "temp_file=%~dp0temp.bat"
+set "copy_lines=0"
+
+REM Copy the main script to the temporary batch file
+echo @echo off>"%temp_file%"
+echo.>>"%temp_file%"
+for /f "delims=" %%i in ('findstr /n "^" "%this_file%"') do (
+    set "line=%%i"
+    set "line=!line:*:=!"
+    if "!line!"==":start_main_script" (
+        set "copy_lines=1"
+    ) else if "!line!"==":end_main_script" (
+        set "copy_lines=0"
+    ) else if !copy_lines! equ 1 (
+        if "!line!"=="" (
+            echo.>>"%temp_file%"
+        ) else (
+            echo !line!>>"%temp_file%"
+        )
+    )
+)
+
+REM Cleanup by deleting the configuration script and replacing this script with the temporary script
+del configure_template.py
 move /Y "%temp_file%" "%this_file%"
