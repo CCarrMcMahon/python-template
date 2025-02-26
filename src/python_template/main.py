bd2c2281866c13734cda3586fd83028ba9ee125d
Christopher McMahon <CCarrMcMahon@gmail.com>
2025-02-26 03:42:10 -0600

    #11: Refactor argument names for logging configuration and add introductory logging function

--- src/python_template/main.py
+++ src/python_template/main.py
@@ -16,22 +16,22 @@ def create_argparser() -> ArgumentParser:
 
     # Optional arguments
     parser.add_argument(
-        "--ll",
+        "--log_lvl",
         type=str,
-        default=LogLevel.INFO.name,
+        default=LogLevel.INFO.name.lower(),
         choices=[level.name.lower() for level in LogLevel],
         help="The logging level to use for the root logger.",
     )
     parser.add_argument(
-        "--lf",
+        "--log_fmt",
         type=str,
-        default=LogFormat.MSECS.name,
+        default=LogFormat.MSECS.name.lower(),
         choices=[format.name.lower() for format in LogFormat],
         help="The logging format to use for the root logger.",
     )
     parser.add_argument(
         "-v",
-        "--verbose",
+        "--log_v",
         action="store_true",
         help="Increase the verbosity of the logging output to include more detailed information.",
     )
@@ -39,23 +39,28 @@ def create_argparser() -> ArgumentParser:
     return parser
 
 
+def log_intro() -> None:
+    """Log a simple introduction message to show logging state."""
+    logger = logging.getLogger(__name__)
+    logger.debug("Debug logging enabled.")
+    logger.info("Hello, World!")
+
+
 def main() -> None:
     """The entry point when running the package directly which will populate and parse any configured arguments."""
     parser = create_argparser()
     args = parser.parse_args()
 
-    log_level = LogLevel[args.ll.upper()]
-    log_format = LogFormat[args.lf.upper()]
-    verbose = args.verbose
+    log_lvl = LogLevel[args.log_lvl.upper()]
+    log_fmt = LogFormat[args.log_fmt.upper()]
+    log_v = bool(args.log_v)
 
-    if verbose:
-        log_level = LogLevel.DEBUG
-        log_format = LogFormat.LINE
+    if log_v:
+        log_lvl = LogLevel.DEBUG
+        log_fmt = LogFormat.LINE
 
-    log_config.initialize_root_logger(log_level, log_format)
-    logger = logging.getLogger(__name__)
-    logger.debug("Debug logging enabled.")
-    logger.info("Hello, World!")
+    log_config.initialize_root_logger(log_lvl, log_fmt)
+    log_intro()
 
 
 if __name__ == "__main__":
