aae78470f2211138f3b115c13661acfb3841b5b5
Christopher McMahon <CCarrMcMahon@gmail.com>
2025-03-15 03:46:27 -0500

    #11: Adjust test_log_intro to handle no messages be logged

--- tests/test_main.py
+++ tests/test_main.py
@@ -19,19 +19,14 @@ def test_log_intro(request: pytest.FixtureRequest, caplog: pytest.LogCaptureFixt
     main.log_intro()
 
     # Get the logging configuration
-    log_level = LogLevel[request.config.getoption("log_lvl").upper()]
-    verbose = request.config.getoption("log_v")
-    if verbose:
-        log_level = LogLevel.DEBUG
+    log_level = LogLevel[request.config.getoption("log_level")]
 
     # Check the logging output
     if log_level == LogLevel.DEBUG:
-        assert caplog.records[-2].message == "Debug logging enabled."
-    assert caplog.records[-1].message == "Hello, World!"
-
-    # Check the logging format
-    if log_level == LogLevel.DEBUG:
-        assert caplog.records[-2].levelname == "DEBUG"
-    assert caplog.records[-1].levelname == "INFO"
-
-    logger.debug("Test complete.")
+        record = caplog.records[-2]
+        assert record.message == "Debug logging enabled."
+        assert record.levelname == "DEBUG"
+    if log_level == LogLevel.DEBUG or log_level == LogLevel.INFO:
+        record = caplog.records[-1]
+        assert record.message == "Hello, World!"
+        assert record.levelname == "INFO"
